eab7fff66cb1e1dbb6cf2a118e256a6a153a75e7
Alex Bates <ajbates93@gmail.com>
2022-07-28 10:03:28 +0100

    feat: vite.config imports and config setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetUno, presetIcons } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components(),
+    AutoImport({
+      imports: [
+        'vue'
+      ],
+      dirs: [
+        './src/composables'
+      ],
+      vueTemplate: true
+    }),
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons()
+      ]
+    })
+  ]
 })

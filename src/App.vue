63c921f301a42966431d8cef84fbb1a25820ae8b
Alex Bates <ajbates93@gmail.com>
2022-07-28 10:43:11 +0100

    feat: add header and more config stuff

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,7 @@
 </script>
 
 <template>
-  
+  <div>
+    <AppHeader />
+  </div>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header bg-gray-200 p3>
+    <div flex justify-start items-center
+      font-bold>
+      <div text-blue-500 text-xl inline-block mr-3>Morning Routine</div>
+      <router-link hover="cursor-pointer underline" to="/"><div icon-btn i-carbon-home></div> Dashboard</router-link>
+      <router-link hover="cursor-pointer underline" ml-auto to="/login"><div icon-btn i-carbon-login></div> Login</router-link>
+    </div>
+  </header>
+</template>
+
+
+<style scoped>
+
+</style>
